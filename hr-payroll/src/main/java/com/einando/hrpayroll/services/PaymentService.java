c03e6a61da634da9c9070e008a4e74b1333e16c1
Fernando Lopes <87499075+einand0@users.noreply.github.com>
2025-11-26 23:26:37 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/einando/hrpayroll/services/PaymentService.java
@@ -0,0 +1,12 @@
+package com.einando.hrpayroll.services;
+
+import com.einando.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(long workerId, int days){
+        return new Payment("Bob", 200.0, days);
+    }
+}
